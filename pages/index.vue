834e3e0258e453bd72300dfb98002754cf2d6855
mrniamster <niteshkmr366@gmail.com>
2020-11-09 11:09:22 +0530

    initial#2

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <!--Section: Content-->
     <section class="">
-      <!-- Section headings -->
+      <!-- Section headings another test  -->
       <h3 class="text-center font-weight-bold mb-5">Latest news</h3>
 
       <!-- Grid row -->
